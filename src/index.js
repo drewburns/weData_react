47f2fc850e16720b3bab31f1a529c02fc522a68c
drewburns <drewburnsbab@gmail.com>
2020-08-24 21:04:02 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
+import "./index.css";
+import Login from "./screens/Login";
+import SignUp from "./screens/SignUp";
+import Home from "./screens/Home";
+import Dashboard from "./screens/Dashboard";
+import ProtectedRoute from "./utility/ProtectedRoute";
+import { GlobalContextProvider } from "./utility/GlobalContext";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <GlobalContextProvider>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={SignUp} />
+        <ProtectedRoute exact={true} path="/dashboard" component={Dashboard} />
+        {/* <ProtectedRoute component={Dashboard} /> */}
+      </Switch>
+    </GlobalContextProvider>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/utility/GlobalContext.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+
+import jwtDecode from "jwt-decode";
+
+const GlobalContext = React
+  .createContext
+  //   (null as unknown) as ContextProps
+  ();
+
+const initialState = { jwt: "", loading: false, user: {}, currentUserID: null };
+
+const GlobalContextProvider = (props) => {
+  const [state, setState] = useState(initialState);
+
+  useEffect(() => {
+    (async () => {
+      // await AsyncStorage.removeItem("id_token");
+      const value = localStorage.getItem("id_token");
+      // console.log(value)
+      if (value !== null) {
+        const decodedToken = jwtDecode(value, { header: false });
+        const currentUserID = decodedToken.sub;
+        setState({
+          jwt: value,
+          user: {},
+          loading: false,
+          currentUserID: currentUserID,
+        });
+      } else {
+        setState({
+          loading: false,
+        });
+      }
+      // If you want to also store it in async storage,
+      // you could always access it here and set the jwt in state
+    })();
+  }, []);
+
+  return (
+    <GlobalContext.Provider value={{ state, setState }}>
+      {props.children}
+    </GlobalContext.Provider>
+  );
+};
+
+export { GlobalContext, GlobalContextProvider };
