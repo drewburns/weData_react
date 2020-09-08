19af74230f734947df1f06b2d7bdd7fa128a5943
drewburns <drewburnsbab@gmail.com>
2020-09-08 18:24:56 -0400

    fix config

--- src/services/config.js
+++ src/services/config.js
@@ -2,6 +2,7 @@
 const config = {
     development : {
         base_url: "http://localhost:8080"
+        // base_url: "http://quotabackend-env.eba-pinm2tpq.us-east-2.elasticbeanstalk.com"
     },
     production: {
         base_url: "http://quotabackend-env.eba-pinm2tpq.us-east-2.elasticbeanstalk.com"
